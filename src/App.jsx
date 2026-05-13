bda98aac1865b575c0005f4a823282a997cadf2b
Merdann <aleroev2000@list.ru>
2026-05-13 21:20:30 +0300

    ф

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import TaskTrackerWork from "./components/taskTracker_listWork/index.jsx";
+import TaskTrackerWork from "./components";
 
 function App() {
     // Состояния
     return (
         <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 py-8">
             <TaskTrackerWork/>
-            {/*<TaskTrackerNotes/>*/}
         </div>
     );}
 export default App;
